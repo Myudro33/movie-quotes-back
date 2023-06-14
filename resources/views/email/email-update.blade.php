b47999e086c434f3516154dcaff5ec6bf5e3b618
Myudro33 <nika.kanashvili@redberry.ge>
2023-06-14 21:01:24 +0400

    wip: add email templates

---
+++ resources/views/email/email-update.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <p align="center" style="margin: 0; font-family: 'Inter', sans-serif;">
+        <a href="http://localhost:8000/update-email/{{ $email }}&{{$new_email}}"
+            style="display: inline-block; font-size: 16px; font-weight: bold; color: #ffffff; background-color: #0FBA68; padding: 10px 30px; border-radius: 5px; text-decoration: none;">Update Email</a>
+    </p>
+</body>
+</html>
