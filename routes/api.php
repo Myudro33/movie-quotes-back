aa3d8c11620b832c17e9bfc470696249d57b917f
Myudro33 <nika.kanashvili@redberry.ge>
2023-06-15 20:48:08 +0400

    feat: add new routes for add like and get quotes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\GoogleAuthController;
+use App\Http\Controllers\LikeController;
 use App\Http\Controllers\PasswordResetController;
+use App\Http\Controllers\QuoteController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\VerificationController;
 use Illuminate\Http\Request;
@@ -21,14 +23,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 	return $request->user();
 });
-Route::group(['controller' => UserController::class], function () {
+Route::controller(UserController::class)->group(function () {
 	Route::post('/register', 'register');
 	Route::post('/login', 'login');
 	Route::post('/logout', 'logout');
 	Route::post('/upload-avatar', 'upload');
 	Route::put('/update-user/{user}', 'update_user');
 });
-Route::group(['controller'=>GoogleAuthController::class], function () {
+Route::controller(GoogleAuthController::class)->group(function () {
 	Route::get('auth/redirect', 'redirect');
 	Route::get('auth/google/callback', 'callback');
 });
@@ -38,8 +40,16 @@ Route::controller(VerificationController::class)->group(function () {
 	Route::get('/password-update/{token}', 'passwordReset');
 });
 
-Route::group(['controller'=>PasswordResetController::class], function () {
+Route::controller(PasswordResetController::class)->group(function () {
 	Route::post('/forgot-password', 'send_reset_password_mail')->name('password.email');
 	Route::put('/password-update/{token}', 'reset_password')->name('password.reset');
 	Route::put('/update-password', 'password_update');
+});
+
+Route::controller(QuoteController::class)->group(function () {
+	Route::get('/quotes', 'get_quotes');
+});
+
+Route::controller(LikeController::class)->group(function () {
+	Route::post('/addLike', 'like');
 });
