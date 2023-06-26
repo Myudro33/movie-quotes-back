9daabb7daa4166cf782a2c2ee62087227171bfdb
Myudro33 <nika.kanashvili@redberry.ge>
2023-06-27 00:47:59 +0400

    refactor: create comment function

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,19 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CommentStoreRequest;
 use App\Http\Resources\CommentResource;
 use App\Models\Comment;
-use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-	public function create(Request $request)
+	public function create(CommentStoreRequest $request)
 	{
-		$comment = Comment::create([
-			'user_id' => $request->user_id,
-			'quote_id'=> $request->quote_id,
-			'title'   => $request->title,
-		]);
+		$comment = Comment::create($request->validated());
 		return response()->json(['message'=>'success', 'comment'=>new CommentResource($comment)], 201);
 	}
 }
