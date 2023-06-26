f56792f3af89e1f5f6371594a7ffefd12fd85068
Myudro33 <nika.kanashvili@redberry.ge>
2023-06-27 00:55:18 +0400

    feat: create resource for genres

--- app/Http/Resources/MovieResource.php
+++ app/Http/Resources/MovieResource.php
@@ -19,7 +19,7 @@ class MovieResource extends JsonResource
 			'name'       => json_decode($this->name),
 			'year'       => $this->year,
 			'image'      => $this->image,
-			'genre'      => json_decode($this->genre),
+			'genre'      => GenresResource::collection(json_decode($this->genres)),
 			'description'=> json_decode($this->description),
 			'director'   => json_decode($this->director),
 			'quotes'     => MovieQuotesResource::collection($this->quotes),
