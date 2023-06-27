5610e05e7db6d76597be898853561e6dd4bcda8a
Myudro33 <nika.kanashvili@redberry.ge>
2023-06-27 12:53:48 +0400

    feat: validate like delete request

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\LikeCreateRequest;
+use App\Http\Requests\LikeDestroyRequest;
 use App\Http\Resources\LikeResource;
 use App\Models\Like;
 use Illuminate\Http\JsonResponse;
@@ -18,7 +19,7 @@ class LikeController extends Controller
 		return response()->json(['message'=>'success', 'like'=>new LikeResource($like)], 201);
 	}
 
-	public function delete(Like $like): JsonResponse
+	public function delete(LikeDestroyRequest $request, Like $like): JsonResponse
 	{
 		$like->delete();
 		return response()->json(['message'=>'like deleted'], 202);
