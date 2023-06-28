0eee0c6bb5442078306ccec243f57deadbb13e0c
Myudro33 <nika.kanashvili@redberry.ge>
2023-06-28 10:26:48 +0400

    refactor: movie genres migration

--- database/migrations/2023_06_26_202927_create_genre_movie_table.php
+++ database/migrations/2023_06_26_202927_create_genre_movie_table.php
@@ -9,8 +9,10 @@ class CreateGenreMovieTable extends Migration
 	public function up()
 	{
 		Schema::create('genre_movie', function (Blueprint $table) {
-			$table->smallInteger('genre_id')->foreign('genre_id')->references('id')->on('genres')->onDelete('cascade');
-			$table->smallInteger('movie_id')->foreign('movie_id')->references('id')->on('movies')->onDelete('cascade');
+			$table->foreignId('genre_id')->constrained('genres')
+			->onDelete('cascade');
+			$table->foreignId('movie_id')->constrained('movies')
+			->onDelete('cascade');
 			$table->timestamps();
 		});
 	}
