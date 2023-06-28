1868b6552543a2311ab5812a4355c8a0a78a9f85
Myudro33 <nika.kanashvili@redberry.ge>
2023-06-28 17:43:56 +0400

    feat: create password forgot request

---
+++ app/Http/Requests/PasswordForgotRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class PasswordForgotRequest extends FormRequest
+{
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+	 */
+	protected function failedValidation(Validator $validator)
+	{
+		throw new HttpResponseException(response()->json(['message'=>'error', 'errors' => $validator->errors()], 422));
+	}
+
+	public function rules(): array
+	{
+		return [
+			'email'=> 'required|email|exists:users,email',
+		];
+	}
+}
