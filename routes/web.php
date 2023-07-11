7f4e21ccd6f07c76e19652ad4adb905d20e818cb
Myudro33 <nika.kanashvili@redberry.ge>
2023-07-11 10:33:19 +0400

    fix: remove swagger schemas

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 	return $request->user();
 });
-Route::view('/', 'swagger')->name('landing');
+Route::view('/', 'welcome')->name('landing');
