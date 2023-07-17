3c6279eb700a0b5992b986f54a3c56513239e7cb
Myudro33 <nika.kanashvili@redberry.ge>
2023-07-17 22:17:39 +0400

    fix: add local urls in sanctum php

--- config/sanctum.php
+++ config/sanctum.php
@@ -16,7 +16,7 @@ return [
 
 	'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
 		'%s%s',
-		'localhost,localhost:3000,localhost:5173,127.0.0.1,127.0.0.1:8000,127.0.0.1:5173,movie-quotes-back.nika-qanashvili.redberryinternship.ge,movie-quotes-front.nika-qanashvili.redberryinternship.ge,::1',
+		'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,127.0.0.1:5174,localhost:5174,movie-quotes-back.nika-qanashvili.redberryinternship.ge,movie-quotes-front.nika-qanashvili.redberryinternship.ge,::1',
 		Sanctum::currentApplicationUrlWithPort()
 	))),
 	/*
