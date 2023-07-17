0453746bb0a6b5a3f123cb240ada27e28d5cec45
Myudro33 <nika.kanashvili@redberry.ge>
2023-07-17 11:18:44 +0400

    fix: modify sanctum php

--- config/sanctum.php
+++ config/sanctum.php
@@ -14,8 +14,11 @@ return [
 	|
 	*/
 
-	'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost,127.0.0.1')),
-
+	'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
+		'%s%s',
+		'localhost,localhost:3000,localhost:5173,127.0.0.1,127.0.0.1:8000,127.0.0.1:5173,movie-quotes-back.nika-qanashvili.redberryinternship.ge,movie-quotes-front.nika-qanashvili.redberryinternship.ge,::1',
+		Sanctum::currentApplicationUrlWithPort()
+	))),
 	/*
 	|--------------------------------------------------------------------------
 	| Sanctum Guards
